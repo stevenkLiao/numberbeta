a9f4859a1e8283901eaba5ec90ad0810eae54cb5
StevenLiao <slamslamslam12411@gmail.com>
2017-07-17 17:36:52 +0800

    7/17-1

--- app/src/main/java/com/kengyu/httpconnect/store/LoginAvtivity.java
+++ app/src/main/java/com/kengyu/httpconnect/store/LoginAvtivity.java
@@ -3,6 +3,7 @@ package com.kengyu.httpconnect.store;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -29,6 +30,14 @@ public class LoginAvtivity extends AppCompatActivity {
                 Intent intent = new Intent( LoginAvtivity.this, qrcode.class);
                 intent.putExtras(bundle);
                 startActivity(intent);
+            }
+        });
+
+        storeText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("EditText","test");
+                storeText.setText("");
             }
         });
     }
